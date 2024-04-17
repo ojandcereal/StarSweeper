7a5c7d04fd90931a4ba817a8be5a8d88976d2070
ojandcereal <113937285+ojandcereal@users.noreply.github.com>
2024-04-16 20:55:16 -0400

    more gravity

--- StarSweeper 5.3/Intermediate/Build/BuildRulesProjects/StarSweeperModuleRules/obj/Development/StarSweeperModuleRules.AssemblyInfo.cs
+++ StarSweeper 5.3/Intermediate/Build/BuildRulesProjects/StarSweeperModuleRules/obj/Development/StarSweeperModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StarSweeperModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac00388c043205738886ebe53875bfacacf91f15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae47b53894d4ef2053e83d529a29fe6f1481c8c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("StarSweeperModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StarSweeperModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
